b3c9af58c0c58ad521a08d9abf04d8e381c8bd3c
songyao.huang <songyao.huang@ambergroup.io>
2021-04-09 17:13:19 +0800

    fix: 修改为使用example作为demo工程

--- number_wheel_text_demo/example/android/app/src/main/kotlin/com/huangsongyao/number/wheel/number_wheel_animation_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.huangsongyao.number.wheel.number_wheel_animation_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- number_wheel_text_demo/ios/Classes/NumberWheelAnimationPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface NumberWheelAnimationPlugin : NSObject<FlutterPlugin>
-@end
